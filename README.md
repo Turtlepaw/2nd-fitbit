d34877421f7297a14873c31cbd613ef70e793664
TurtlePaw <81275769+Turtlepaw@users.noreply.github.com>
2024-01-27 23:36:59 -0500

    feat(app): updates

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# 2nd-fitbit
+# 2nd Fitbit
+
 ✨ Manage &amp; sync two fitbits
+
+## 🤔 What's this project?
+
+It's simple, if you have two fitbits and want to sync them to one fitbit account, this app does that.
+
+## 📦 How do I set it up?
+
+1. Download the [latest release][1] APK
+2. Install the APK downloaded
+
+## 🔒 Is it safe?
+
+Yup, it's safe, but don't take my word for it, take a look at the source code, it's all available here.
+
+[1]: https://github.com/Turtlepaw/2nd-fitbit/releases/latest
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs, router } from "expo-router";
+import { Pressable } from "react-native";
+
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { MaterialIcons } from "@expo/vector-icons";
+import { BottomNavigation, useTheme } from "react-native-paper";
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof MaterialIcons>["name"];
+  color: string;
+}) {
+  return <MaterialIcons size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const theme = useTheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.colors.background,
+        },
+        headerTitleStyle: {
+          color: theme.colors.onBackground,
+        },
+      }}
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          navigationState={state}
+          safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+              // navigation.dispatch({
+              //   ...CommonActions.navigate(route.name, route.params),
+              //   target: state.key,
+              // });
+              router.push(
+                (route.name == "index" ? "/" : `/${route.name}`) as any
+              );
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel.toString()
+                : options.title !== undefined
+                ? options.title
+                : route.name;
+
+            return label;
+          }}
+        />
+      )}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Tab One",
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: "Tab Two",
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
